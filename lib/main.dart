601750892a89f8923a7765578bbff5641326f732
Alberto Pedron <albertop2197@gmail.com>
2024-07-09 23:32:37 +0200

    main: Flutter Demo -> eGuasti

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'eGuasti',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
